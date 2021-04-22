482a728a346e2765db83061221dda79576216de9
Souvik Biswas <sbis1999@gmail.com>
2021-04-23 04:35:13 +0530

    Building login page

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Authentication'),
+      ),
+    );
+  }
+}
