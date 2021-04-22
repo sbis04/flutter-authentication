5f87ea027d7b224946631f2089cd35df9b12154b
Souvik Biswas <sbis1999@gmail.com>
2021-04-23 04:54:35 +0530

    Add initialization

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,11 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Firebase Authentication'),
+      ),
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              children: [
+                Text('Login'),
+              ],
+            );
+          }
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
