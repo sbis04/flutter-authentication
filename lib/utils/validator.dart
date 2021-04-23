be157279dafc55d133410cee45a829006bc61944
Souvik Biswas <sbis1999@gmail.com>
2021-04-23 18:43:27 +0530

    Add validators

---
+++ lib/utils/validator.dart
@@ -0,0 +1,44 @@
+class Validator {
+  static String? validateName({required String? name}) {
+    if (name == null) {
+      return null;
+    }
+
+    if (name.isEmpty) {
+      return 'Name can\'t be empty';
+    }
+
+    return null;
+  }
+
+  static String? validateEmail({required String? email}) {
+    if (email == null) {
+      return null;
+    }
+
+    RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+
+    if (email.isEmpty) {
+      return 'Email can\'t be empty';
+    } else if (!emailRegExp.hasMatch(email)) {
+      return 'Enter a correct email';
+    }
+
+    return null;
+  }
+
+  static String? validatePassword({required String? password}) {
+    if (password == null) {
+      return null;
+    }
+
+    if (password.isEmpty) {
+      return 'Password can\'t be empty';
+    } else if (password.length < 6) {
+      return 'Enter a password with length at least 6';
+    }
+
+    return null;
+  }
+}
