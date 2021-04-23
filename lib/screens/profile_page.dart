f09dbac54712849c403c36275ce6be6a63c414d4
Souvik Biswas <sbis1999@gmail.com>
2021-04-23 18:42:53 +0530

    Add profile page

---
+++ lib/screens/profile_page.dart
@@ -0,0 +1,126 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_authentication/screens/login_page.dart';
+import 'package:flutter_authentication/utils/fire_auth.dart';
+
+class ProfilePage extends StatefulWidget {
+  final User user;
+
+  const ProfilePage({required this.user});
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  bool _isSendingVerification = false;
+  bool _isSigningOut = false;
+
+  late User _currentUser;
+
+  @override
+  void initState() {
+    _currentUser = widget.user;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'NAME: ${_currentUser.displayName}',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'EMAIL: ${_currentUser.email}',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            SizedBox(height: 16.0),
+            _currentUser.emailVerified
+                ? Text(
+                    'Email verified',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.green),
+                  )
+                : Text(
+                    'Email not verified',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1!
+                        .copyWith(color: Colors.red),
+                  ),
+            SizedBox(height: 16.0),
+            _isSendingVerification
+                ? CircularProgressIndicator()
+                : Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            _isSendingVerification = true;
+                          });
+                          await _currentUser.sendEmailVerification();
+                          setState(() {
+                            _isSendingVerification = false;
+                          });
+                        },
+                        child: Text('Verify email'),
+                      ),
+                      SizedBox(width: 8.0),
+                      IconButton(
+                        icon: Icon(Icons.refresh),
+                        onPressed: () async {
+                          User? user = await FireAuth.refreshUser(_currentUser);
+
+                          if (user != null) {
+                            setState(() {
+                              _currentUser = user;
+                            });
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+            SizedBox(height: 16.0),
+            _isSigningOut
+                ? CircularProgressIndicator()
+                : ElevatedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isSigningOut = true;
+                      });
+                      await FirebaseAuth.instance.signOut();
+                      setState(() {
+                        _isSigningOut = false;
+                      });
+                      Navigator.of(context).pushReplacement(
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                    },
+                    child: Text('Sign out'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+}
